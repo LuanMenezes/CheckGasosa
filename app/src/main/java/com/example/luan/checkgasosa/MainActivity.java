7b48d2ba6dd7d0e740c0d311fdbca7f8bba306bf
Luan Menezes <luan_menezes90@hotmail.com>
2017-05-13 18:40:43 -0300

    criado classes para as activitys

--- app/src/main/java/com/example/luan/checkgasosa/MainActivity.java
+++ app/src/main/java/com/example/luan/checkgasosa/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.luan.checkgasosa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Chamada para a view de visualizacao dos registros*/
+    public void visualizar(View v){
+        Intent intent = new Intent(this, VisualizarActivity.class);
+        startActivity(intent);
+    }
+
+    /** Chamada para a view de Cadastro de registro*/
+    public void cadastrar(View v) {
+        Intent intent = new Intent(this, CadastrarActivity.class);
+        startActivity(intent);
     }
 }
